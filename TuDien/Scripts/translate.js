b8f43cd6e1e819aba8ab6ea8f3a196caa6c12d2f
pta208 <pta20801@gmail.com>
2022-04-10 21:44:54 +0700

    add eventlistener to addWord buttons dynamically

--- TuDien/Scripts/translate.js
+++ TuDien/Scripts/translate.js
@@ -1,4 +1,10 @@
-﻿const searchWord = () => {
+﻿let outputList = document.querySelector(".output-list");
+let data = {};
+let addWordButton = document.createElement("button");
+addWordButton.innerHTML = '<button class="btn btn-submit" onclick="addWordButtonAddEvent()">Lưu từ</button>';
+const searchWord = () => {
+
+    outputList.innerHTML = "";
     let tiengAnh = document.querySelector("#tiengAnhTxt").value.toLowerCase();
     const url =
         `https://dictionary-api-five.vercel.app/api/v1/entries/vi/` + tiengAnh;
@@ -6,49 +12,48 @@
     const getData = fetch(url)
         .then((response) => response.json())
         .then((wordsOutput) => {
-            let resHTML = "";
             wordsOutput.forEach((word) => {
-                const data = {
-                    ngayThem: Date.now(),
+                data = {
+                    ngayThem: new Date().toLocaleDateString(),
                     tiengAnh: tiengAnh,
                     tiengViet: word.translate,
                     thongTinThem: word.meanings[0].definitions[0].examples,
                     loaiTu: word.type,
                 }
-                let examples = [];
-                word.meanings.forEach((meaning) => {
-                    examples.push(meaning.definitions[0].examples);
-                });
-                let examplesHTML = "";
-                for (var i = 0; i < examples.length; i++) {
-                    examplesHTML +=
-                        `<li class="word-example-item"><p>` + examples[i] + `</p></li>`;
-                }
-                var html = `
+            });
+            createOutputItem(data);
+        });
+
+};
+
+const createOutputItem = (data) => {
+    let resHTML = "";
+    let examplesHTML = 
+            `<li class="word-example-item"><p>` + data.thongTinThem + `</p></li>`;
+    var html = `
                       <li class="output-item container px-3 py-2 my-2">
-                        <h6 class="word-type">${word.type}</h6>
-                        <div class="word-meaning">${word.translate}</div>
+                        <h6 class="word-type">${data.loaiTu}</h6>
+                        <div class="word-meaning">${data.tiengViet}</div>
                         <h6>Ví dụ:</h6>
                         <ul class="word-example-list"> 
                               ${examplesHTML}
                         </ul>
                         <div class="word-action w-100 text-end">
-                          <button class="btn btn-submit" onclick="addWord(${data})>Lưu từ</button>
+                              ${addWordButton.innerHTML}
                         </div>
                       </li>
                     `;
-                resHTML += html;
-            });
-
-            outputList = document.querySelector(".output-list");
-            outputList.innerHTML = resHTML;
-        });
-};
+    resHTML += html;
+    outputList.innerHTML += (resHTML);
+}
+const addWordButtonAddEvent = () => {
+    addWordButton.addEventListener("click", addWord(data), false);
+}
 let buttonSearch = document.querySelector("#btn-search");
 buttonSearch.addEventListener("click", searchWord);
 
 const addWord = (data) => {
-    let url = `https://localhost:44321/?ngayThem=${data.ngayThem}&tiengAnh=${data.tiengAnh}&tiengViet=${data.tiengViet}&thongTinThem=${data.thongTinThem}&loaiTu=${data.loaiTu}`
+    let url = `https://localhost:44321/api/Word/?ngayThem=${data.ngayThem}&tiengAnh=${data.tiengAnh}&tiengViet=${data.tiengViet}&thongTinThem=${data.thongTinThem}&loaiTu=${data.loaiTu}`;
     const pushWord = fetch(url, {
         method: 'POST', // or 'PUT'
         })
