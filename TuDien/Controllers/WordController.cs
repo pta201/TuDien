147ae09c8637d1af079d8781ebd22cdda50385f7
pta208 <pta20801@gmail.com>
2022-04-10 11:36:59 +0700

    Added "Word" api controller, front-end page to fetch api

---
+++ TuDien/Controllers/WordController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TuDien.Models;
+
+namespace TuDien.Controllers
+{
+    public class WordController : ApiController
+    {
+        WordDataContext db = new WordDataContext();
+        //Get all
+        [HttpGet]
+        public List<TiengAnh_TiengViet> getAll()
+        {
+            List<TiengAnh_TiengViet> wordList = new List<TiengAnh_TiengViet>();
+            var words = db.TiengAnh_TiengViets.ToList();
+            foreach(var word in words)
+            {
+                wordList.Add(new TiengAnh_TiengViet(word.NgayThem, word.TiengAnh, word.TiengViet, word.ThongTinThem, word.DoKho ));
+            }
+            return wordList;
+        }
+        [HttpGet]
+        public TiengAnh_TiengViet getByID(int id)
+        {
+            TiengAnh_TiengViet word = db.TiengAnh_TiengViets.SingleOrDefault(n => n.id == id);
+            return word;
+        }
+        [HttpPost]
+        [HttpGet]
+        public TiengAnh_TiengViet getByEnglish(string tiengAnh)
+        {
+            TiengAnh_TiengViet word = db.TiengAnh_TiengViets.SingleOrDefault(n => n.TiengAnh == tiengAnh);
+            return word;
+        }
+        [HttpPost]
+        public bool addWord(DateTime ngayThem, string tiengAnh, string tiengViet, string thongTinThem, string doKho)
+        {
+            try
+            {
+                TiengAnh_TiengViet word = new TiengAnh_TiengViet();
+                word.NgayThem = ngayThem;
+                word.TiengAnh = tiengAnh;
+                word.TiengViet = tiengViet;
+                word.ThongTinThem = thongTinThem;
+                word.DoKho = doKho;
+                db.TiengAnh_TiengViets.InsertOnSubmit(word);
+                db.SubmitChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        [HttpPut]
+        public bool updateWord(int id, DateTime ngayThem, string tiengAnh, string tiengViet, string thongTinThem, string doKho)
+        {
+            try
+            {
+                TiengAnh_TiengViet word = db.TiengAnh_TiengViets.SingleOrDefault(n => n.id == id);
+                word.NgayThem = ngayThem;
+                word.TiengAnh = tiengAnh;
+                word.TiengViet = tiengViet;
+                word.ThongTinThem = thongTinThem;
+                word.DoKho = doKho;
+                db.SubmitChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ TuDien/Scripts/translate.js
@@ -0,0 +1,45 @@
+﻿const searchWord = () => {
+    let tiengAnh = document.querySelector("#tiengAnhTxt").value.toLowerCase();
+    const url =
+        `https://dictionary-api-five.vercel.app/api/v1/entries/vi/` + tiengAnh;
+
+    const getData = fetch(url)
+        .then((response) => response.json())
+        .then((wordsOutput) => {
+            let resHTML = "";
+            wordsOutput.forEach((word) => {
+                let examples = [];
+                console.log(word);
+                word.meanings.forEach((meaning) => {
+                    examples.push(meaning.definitions[0].examples);
+                });
+                let examplesHTML = "";
+                for (var i = 0; i < examples.length; i++) {
+                    examplesHTML +=
+                        `<li class="word-example-item"><p>` + examples[i] + `</p></li>`;
+                }
+                var html = `
+                  <li class="output-item container px-3 py-2 my-2">
+                    <h6 class="word-type">${word.type}</h6>
+                    <div class="word-meaning">${word.translate}</div>
+                    <h6>Ví dụ:</h6>
+                    <ul class="word-example-list"> 
+                          ${examplesHTML}
+                    </ul>
+                    <div class="word-action w-100 text-end">
+                      <button class="btn btn-submit">Lưu từ</button>
+                    </div>
+                  </li>
+          `;
+                resHTML += html;
+            });
+
+            outputList = document.querySelector(".output-list");
+            outputList.innerHTML = resHTML;
+        });
+};
+//   const timeElapsed = Date.now();
+//   const today = new Date(timeElapsed);
+//   today.toLocaleDateString();
+let buttonSearch = document.querySelector("#btn-search");
+buttonSearch.addEventListener("click", searchWord);
